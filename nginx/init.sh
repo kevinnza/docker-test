31682451bcf7c772458dcdf4f32fb7474ddb90b7
kevinnza <kevinnza@gmail.com>
2018-11-21 12:44:49 +0200

    Update init.sh

--- nginx/init.sh
+++ nginx/init.sh
@@ -14,6 +14,9 @@ then
         certbot -n -q certonly --standalone --email ${SETUP_LETS_ENCRYPT_EMAIL} --agree-tos --rsa-key-size 4096 --domains ${SETUP_LETS_ENCRYPT_DOMAINS} --cert-path /etc/ssl/certs/sitecert.pem --key-path /etc/ssl/private/sitecert.key
         wait
     fi
+    
+    echo "      ssl_certificate     /etc/letsencrypt/live/${SETUP_LETS_ENCRYPT_DOMAINS}/fullchain.pem;" > /etc/nginx/ssl-cert.conf
+    echo "      ssl_certificate_key /etc/letsencrypt/live/${SETUP_LETS_ENCRYPT_DOMAINS}/privkey.pem;" >> /etc/nginx/ssl-cert.conf
 fi
 
 exec nginx -g "daemon off;"
